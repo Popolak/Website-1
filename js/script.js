ef982481179b8e849fc3e62c7c45413f18dc964a
sammour <sammour>
2017-10-12 10:31:39 +0200

    fix monkey 3 / bigger

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $( document ).ready(function() {
     // Events
     rightMonkeyTarget.mouseenter(function () {
         $(this).stop(true,false).animate({
-            "right": "-5%"
+            "right": "-6%"
         }, 1000, function () {
             // Nothing
         });
